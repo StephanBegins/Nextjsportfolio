ac204d94a9380517024036f5a1e4ef872a58b5bd
Stephan <stephan1217at@gmail.com>
2024-08-23 23:29:58 +0530

    added

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+// components/Header.jsx
+import Link from "next/link";
+import Button from "./ui/button"; // Ensure this path is correct
+
+
+// components
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Stephan<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+
+        {/* desktop nov & hire me button*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>   
+
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
