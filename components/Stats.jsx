76b31978e5603504323ce619b08ccfbfc182747a
StephanBegins <stephan1217at@gmail.com>
2024-11-12 18:53:39 +0530

    working on stats in home page

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,5 +1,47 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = (
+    {
+        num: 12,
+        text: "Years of Experience",
+    },
+    {
+        num: 26,
+        text: "Projects Completed",
+    },
+    {
+        num: 8,
+        text: "Technologies mastered",
+    },
+    {
+        num: 500,
+        text: "Code commits",
+    },
+)
+
 const Stats = () => {
-    return <div>Stats</div>;
+    return (
+        <section>
+            <div className="container mx">
+                <div>
+                    {stats.map((item, index)=>{
+                        return ( 
+                            <div key={index}>
+                                <CountUp 
+                                    end={item.num} 
+                                    duration={5} 
+                                    delay={2} 
+                                    className="text-4xl xl:text-6xl font-extrabold" 
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Stats;
