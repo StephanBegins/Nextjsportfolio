e751609901b8d2f20cb577cf95cf88faa8a42819
StephanBegins <stephan1217at@gmail.com>
2024-11-12 19:28:02 +0530

    Everything in Home page finished developed as of YT Video

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -2,7 +2,7 @@
 
 import CountUp from "react-countup";
 
-const stats = (
+const stats = [
     {
         num: 12,
         text: "Years of Experience",
@@ -19,22 +19,33 @@ const stats = (
         num: 500,
         text: "Code commits",
     },
-)
+];
 
 const Stats = () => {
     return (
-        <section>
-            <div className="container mx">
-                <div>
-                    {stats.map((item, index)=>{
+        <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+            <div className="container mx-auto">
+                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none"> 
+                    {stats.map((item, index) => {
                         return ( 
-                            <div key={index}>
+                            <div 
+                                className="flex-1 flex gap-4 items-center justify-center 
+                                xl:justify-start" 
+                                key={index}
+                            >
                                 <CountUp 
                                     end={item.num} 
                                     duration={5} 
                                     delay={2} 
-                                    className="text-4xl xl:text-6xl font-extrabold" 
+                                    className="text-5xl xl:text-6xl font-extrabold" 
                                 />
+                                <p 
+                                    className={`${
+                                        item.text.length < 15 ? "max-w-[100px]": "max-w-[150px]"
+                                    } leading-snug text-gray-400/80`}
+                                >
+                                    {item.text}
+                                </p>
                             </div>
                         );
                     })}
